3f72383ea4b58266ae9268252c39fdb9d763b8a3
mdulac <melchior.dulac@inra.fr>
2019-12-13 14:42:08 +0100

    small change to thermp

--- rpTool.py
+++ rpTool.py
@@ -31,10 +31,12 @@ def calculateGlobalScore(rpsbml,
         ####### rule_score ###########
         brsynth_dict = rpsbml.readBRSYNTHAnnotation(annot)
         all_rule_score += brsynth_dict['rule_score']
+        #print('\trule_score: '+str(brsynth_dict['rule_score']))
         ####### selenzyme ############
         try:
             #TODO: do a mean of the topX selenzyme score
             top_selenzyme += brsynth_dict['selenzyme'][sorted(brsynth_dict['selenzyme'], key=lambda kv: kv[1])[0]]
+            #print('\tselenzyme: '+str(brsynth_dict['selenzyme'][sorted(brsynth_dict['selenzyme'], key=lambda kv: kv[1])[0]]))
         except (IndexError, TypeError) as e:
             logging.warning('We are missing a selenzyme values')
     ##############################
@@ -42,12 +44,14 @@ def calculateGlobalScore(rpsbml,
     ##############################
     #lower is better
     norm_rule_score = all_rule_score/len(members)
+    #print('rule_score: '+str(norm_rule_score))
     norm_rule_score = 1.0-norm_rule_score
     #print('rule_score: '+str(norm_rule_score))
     ##############################
     ####### selenzyme ############
     ##############################
     #higher is better
+    #NOTE: that missing selenzyme value will be considered 0.0 since we divide by the number of members
     norm_selenzyme = (top_selenzyme/float(len(members)))/100.0
     #print('selenzyme: '+str(norm_selenzyme))
     ##############################
@@ -68,6 +72,7 @@ def calculateGlobalScore(rpsbml,
                 #if the min is -999999.0 and max 999999.0 # ONLY use if reactions are reversible
                 #norm_fba = (float(child.getAttrValue('value'))+999999.0)/(999999.0+999999.0)
                 #if the min is 0.0 and max 999999.0
+                #print('FBA: '+str(round(float(child.getAttrValue('value')), 4)))
                 norm_fba = round(float(child.getAttrValue('value')), 4)/999999.0
     except (KeyError, TypeError) as e:
         norm_fba = 0.0
@@ -81,13 +86,16 @@ def calculateGlobalScore(rpsbml,
     annot = rp_group.getAnnotation()
     brsynth_dict = rpsbml.readBRSYNTHAnnotation(annot)
     try:
-        if 8901.2>brsynth_dict['dfG_prime_o']['value']>-7570.2:
+        if 8901.2>=brsynth_dict['dfG_prime_o']['value']>=-7570.2:
             #if min is -7570.2 and max is 8901.2
+            #print('thermo: '+str(brsynth_dict['dfG_prime_o']['value']))
             norm_thermo = (brsynth_dict['dfG_prime_o']['value']+7570.2)/(8901.2+7570.2)
             #if min is 8901.2 and max is -7570.2
             #WARNING: Does not work
             #norm_thermo = (brsynth_dict['dfG_prime_o']['value']+8901.2)/(-7570.2-8901.2)
-        else:
+        elif brsynth_dict['dfG_prime_o']['value']<-7570.2:
+            norm_thermo = 0.0
+        elif brsynth_dict['dfG_prime_o']['value']>8901.2:
             norm_thermo = 1.0
     except (KeyError, TypeError) as e:
         norm_thermo = 1.0
@@ -97,6 +105,7 @@ def calculateGlobalScore(rpsbml,
     ##### length of members ####
     ############################
     #lower is better
+    #print('steps: '+str(len(members)))
     if len(members)>max_rp_steps:
         logging.warning('There are more steps than specified')
         norm_steps = 1.0
